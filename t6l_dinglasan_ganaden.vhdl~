1bc95f3a5f1129e2e8d0673b0593d8681f70365b
dsdinglasan <Veigny@gmail.com>
2016-03-25 23:38:18 +0800

    Edited the test bench file and added documentation to both main and test bench file

--- t6l_dinglasan_ganaden.vhdl~
+++ t6l_dinglasan_ganaden.vhdl~
@@ -1,3 +1,6 @@
+-- Dinglasan, Danika Mae
+-- Ganaden, Ma. Patricia
+-- CMSC 132 T-6L
 
 -- Library Statements
 library IEEE; use IEEE.std_logic_1164.all;
@@ -5,7 +8,7 @@ library IEEE; use IEEE.std_logic_1164.all;
 -- Entity Definition
 entity t6l_dinglasan_ganaden is
 	port (z: out std_logic; -- data output
-	 A: in std_logic;
+	 A: in std_logic;--data input
 	 B: in std_logic;
 	 C: in std_logic;
 	 D: in std_logic;
@@ -13,9 +16,9 @@ entity t6l_dinglasan_ganaden is
 	 F: in std_logic);  			
 end entity t6l_dinglasan_ganaden;
 
-architecture buzzer of t6l_dinglasan_ganaden is
+architecture buzzer of t6l_dinglasan_ganaden is --declaration of the architecture
 
 begin
-	z <= (E AND F) OR (D AND E) OR (C AND F) OR (C AND D) OR (B AND E) OR (B AND C) OR (A AND F) OR (A AND D) OR (A AND B);
+	z <= (E AND F) OR (D AND E) OR (C AND F) OR (C AND D) OR (B AND E) OR (B AND C) OR (A AND F) OR (A AND D) OR (A AND B); -- boolean expression for the output
 end architecture buzzer;
 
--- t6l_dinglasan_ganaden_tb.vhdl
+++ t6l_dinglasan_ganaden_tb.vhdl
@@ -1,28 +1,28 @@
--- Dinglasan, Danika Mae 2013-52048
+-- Dinglasan, Danika Mae
 -- Ganaden, Ma. Patricia
 -- CMSC 132 T-6L
 
--- Library Statements
+																			-- Library Statements
 library IEEE; use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
--- Entity Definition
+																			-- Entity Definition
 entity t6l_dinglasan_ganaden_tb is
 	
 end entity t6l_dinglasan_ganaden_tb;
 
 architecture t6l_dinglasan_ganaden_tb of t6l_dinglasan_ganaden_tb is
-	signal z: std_logic;	-- data output
-	signal A: std_logic;
+	signal z: std_logic;													-- data output
+	signal A: std_logic;													-- data inputs
 	signal B: std_logic;
 	signal C: std_logic;
 	signal D: std_logic;
 	signal E: std_logic;
 	signal F: std_logic;
-	-- Component declaration
+																			-- Component declaration
 	component t6l_dinglasan_ganaden is
-		port (z: out std_logic; -- data output
-		 A: in std_logic;
+		port (z: out std_logic;												-- data output
+		 A: in std_logic;													-- data inputs
 		 B: in std_logic;
 		 C: in std_logic;
 		 D: in std_logic;
@@ -30,31 +30,31 @@ architecture t6l_dinglasan_ganaden_tb of t6l_dinglasan_ganaden_tb is
 		 F: in std_logic);  			
 	end component t6l_dinglasan_ganaden;
 
-	begin
+	begin																	-- start of the architecture
 		buzzer: component t6l_dinglasan_ganaden port map(z,A,B,C,D,E,F);
-		process
-			variable error_count: integer:=0;
+		process																-- declaration of the process
+			variable error_count: integer:=0;								-- declaration of the variables
 			variable inputs: unsigned (5 downto 0);
 			variable expected: std_logic;
-			begin
-				inputs:= B"000000";
+			begin															-- start of the process
+				inputs:= B"000000";											-- initialize the input
 				for count in 0 to 63 loop
-					A <= inputs(5);
+					A <= inputs(5);											-- assign each bit to the input variables (signals)
 					B <= inputs(4);
 					C <= inputs(3);
 					D <= inputs(2);
 					E <= inputs(1);
 					F <= inputs(0);
-					wait for 10 ns;
-					expected := (E AND F) OR (D AND E) OR (C AND F) OR (C AND D) OR (B AND E) OR (B AND C) OR (A AND F) OR (A AND D) OR (A AND B);--answer
-					assert(expected = z) report "OR gate design error!" severity error;
-					if(z /= expected) then
+					wait for 10 ns;											-- expected value is the boolean expression of output
+					expected := (E AND F) OR (D AND E) OR (C AND F) OR (C AND D) OR (B AND E) OR (B AND C) OR (A AND F) OR (A AND D) OR (A AND B);
+					assert(expected = z) report "OR gate design error!" severity error; -- check if the expected value is equal to output z
+					if(z /= expected) then --if output is not equal to expected, increment error count
 						error_count:= error_count + 1;
 					end if;
-					inputs := inputs + 1;
+					inputs := inputs + 1; --updates the input
 				end loop;
-				report "Done with test. Errors: " & integer'image(error_count);
+				report "Done with test. Errors: " & integer'image(error_count); -- displays the number of errors
 				wait;
 	end process;
-end architecture t6l_dinglasan_ganaden_tb;
+end architecture t6l_dinglasan_ganaden_tb; -- end of the architecture
 
